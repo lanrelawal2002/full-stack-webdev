dbfed0fb3e8e280a06e564f84504c7fc10ea606b
lanrelawal0022 <lanrelawal2002@gmail.com>
2024-09-24 16:08:58 +0100

    end of DOM Manipulation practice

--- exercise.js
+++ exercise.js
@@ -3,15 +3,38 @@
 // 1) Select the two <button> elements and store them in two different variables.
 //    - Select the first button without adding or using any "id"
 //    - Select the second button by using an "id"
+
+const firstButton = document.querySelector("button");
+
+const secondButton = document.getElementById("second-button");
+
+const firstParagraph = document.body.children[2].children[1];
+
+const thirdParagraph = document.body.children[2].children[3];
+
 // 2) Add "click" event listener to both buttons (with two different functions).
 //    The functions should "console.dir()" the clicked buttons.
 //    - Output the first button by using the variable in which it's stored
 //    - Output the second button WITHOUT using the variable in which it's stored
+
+function firstButtonClicks() {
+  thirdParagraph.remove();
+}
+
+function secondButtonClick() {
+  firstParagraph.classList.add("definition");
+}
+
+firstButton.addEventListener("click", firstButtonClicks);
+
+secondButton.addEventListener("click", secondButtonClick);
+
 // 3) Now select and store the paragraphs mentioned in the text you see on the page
 //    (first and third paragraph)
 //    - Select BOTH paragraphs by drilling into the document and "navigating" to the
 //      mentioned elements
 //    - If you struggle with DOM drilling, use "ids" instead but watch the solution!
+
 // 4) Change the functions from (2) such that:
 //    - The first button removes the third paragraph (i.e. the <p> prior to it)
 //    - The second button changes the background color of the first paragraph to blue
