7f5dd41e981216c6d1130be7ea7f81ba78b56e00
lanrelawal0022 <lanrelawal2002@gmail.com>
2024-11-11 13:39:12 +0100

    added script files and first event listeners

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+editPlayer1BtnElement.addEventListener("click");
