0fd3b7ccfa9c69c3541bdd77faa660873d117b30
lanrelawal0022 <lanrelawal2002@gmail.com>
2024-11-14 10:42:25 +0100

    handled form submission with JS

--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,19 @@ function closePlayerConfig() {
   playerConfigOverlayElement.style.display = "none";
   backdropElement.style.display = "none";
 }
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  //   console.dir(event);
+  const playerNameConfigFormData = new FormData(event.target);
+  //   const playerNameConfigForm = new FormData(formElement);
+  //   console.log(typeof playerNameConfigForm);
+
+  const enteredPlayerName = playerNameConfigFormData.get("playername");
+  console.log(enteredPlayerName); // same as console.dir in this case
+  //   console.log(typeof enteredPlayerName);
+
+  const enteredPlayerBio = playerNameConfigFormData.get("playerbio");
+  console.log(enteredPlayerBio);
+  //   console.log(typeof enteredPlayerBio);
+}
