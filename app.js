cfe4d6eab265ced36c9556f0ebc5f30b34d6fc87
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-02-12 09:07:32 +0100

    getting bk4 ultra ready

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const userName = "Lanny";
-const userNumber = 33;
+// const userName = "Lanny";
+// const userNumber = 33;
 
-console.log(userName, typeof userName);
-console.log(typeof userNumber);
+// console.log(userName, typeof userName);
+// console.log(typeof userNumber);
+
+const http = require("http");
+
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<p>" + new Date().toISOString() + "</p>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<p>" + "Brilliant. You reached the correct port." + "</p>");
+  }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
