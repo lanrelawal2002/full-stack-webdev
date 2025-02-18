f2d0f4e522c068c7d6e082126b2f9ff719ac0860
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-02-18 15:28:10 +0100

    parsing user data with express

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/currenttime", function (req, res) {
   res.send("<p>" + new Date().toISOString() + "</p>");
 });
 
 app.get("/", function (req, res) {
-  res.send("<p>Hello World!</p>");
+  res.send(
+    "<form action='/store-user' method='POST'><label for='express'>Your Name</label><input id='express' type='text' name='username' /><button type='submit'>Submit Form</button></form>"
+  );
+});
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+  console.log("The username is", userName);
+  console.log(req.body);
+  res.send(
+    "<h1 style='color: green; font-style: italic;'>Your name was successfully submitted!</h1>"
+  );
 });
 
 app.listen(3000);
