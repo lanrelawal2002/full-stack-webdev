6b19cc3c5eb1ff8c5db331001b32f32eb3156955
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-03-21 13:57:25 +0100

    using the express router

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const fs = require("node:fs");
 const path = require("node:path");
 
 const express = require("express");
 const uuid = require("uuid");
 
-const restaurantPackage = require("./utilities/restaurant-info/restaurant-data");
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -14,60 +14,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  res.status(200).render("index");
-});
-
-app.get("/restaurants", function (req, res) {
-  const storedRestaurants = restaurantPackage.getStoredRestaurants();
-
-  res.status(200).render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/restaurants/:rid", function (req, res) {
-  const restaurantId = req.params.rid;
-  // console.log(typeof restaurantId);
-
-  const storedRestaurants = restaurantPackage.getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if (restaurant.id === restaurantId) {
-      return res
-        .status(200)
-        .render("restaurant-detail", { randomRestaurant: restaurant });
-    }
-  }
-
-  res.status(404).render("404");
-});
-
-app.get("/recommend", function (req, res) {
-  res.status(200).render("recommend");
-});
-
-app.get("/about", function (req, res) {
-  res.status(200).render("about");
-});
-
-app.get("/confirm", function (req, res) {
-  res.status(200).render("confirm");
-});
-
-app.post("/new-restaurant", function (req, res) {
-  const newRestaurant = req.body;
-  newRestaurant.id = uuid.v4();
-
-  const allMyRestaurants = restaurantPackage.getStoredRestaurants();
-
-  allMyRestaurants.push(newRestaurant);
-
-  restaurantPackage.writeToJsonFile(allMyRestaurants);
-
-  res.redirect("/confirm");
-});
+app.use("/", defaultRoutes);
+app.use("/", restaurantRoutes);
 
 app.use(function (req, res) {
   res.status(404).render("404");
---
+++ routes/restaurants.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+
+const uuid = require("uuid");
+
+const restaurantPackage = require("../utilities/restaurant-info/restaurant-data");
+
+router.get("/restaurants", function (req, res) {
+  const storedRestaurants = restaurantPackage.getStoredRestaurants();
+
+  res.status(200).render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+router.get("/restaurants/:rid", function (req, res) {
+  const restaurantId = req.params.rid;
+  // console.log(typeof restaurantId);
+
+  const storedRestaurants = restaurantPackage.getStoredRestaurants();
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res
+        .status(200)
+        .render("restaurant-detail", { randomRestaurant: restaurant });
+    }
+  }
+
+  res.status(404).render("404");
+});
+
+router.get("/recommend", function (req, res) {
+  res.status(200).render("recommend");
+});
+
+router.get("/confirm", function (req, res) {
+  res.status(200).render("confirm");
+});
+
+router.post("/new-restaurant", function (req, res) {
+  const newRestaurant = req.body;
+  newRestaurant.id = uuid.v4();
+
+  const allMyRestaurants = restaurantPackage.getStoredRestaurants();
+
+  allMyRestaurants.push(newRestaurant);
+
+  restaurantPackage.writeToJsonFile(allMyRestaurants);
+
+  res.redirect("/confirm");
+});
+
+module.exports = router;
