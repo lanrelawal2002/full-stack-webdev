44d3f58d2b2acfb707a2afb164c1b5d2a23fa503
lanrelawal0022 <lanrelawal2002@gmail.com>
2025-04-16 13:05:59 +0100

    diplay post detail page

--- routes/blog.js
+++ routes/blog.js
@@ -9,11 +9,29 @@ router.get("/", function (req, res) {
 });
 
 router.get("/posts", async function (req, res) {
-  const postQuery = `SELECT posts.title, posts.summary, authors.name FROM blog.posts 
+  const postQuery = `SELECT blog.posts.*, blog.authors.name FROM blog.posts 
   INNER JOIN authors ON posts.author_id = authors.id`;
   const result = await db.query(postQuery);
   const [selectedPosts] = result;
-  res.status(200).render("posts-list", { usefulPosts: selectedPosts });
+  const totalPosts = selectedPosts.length;
+  res.status(200).render("posts-list", {
+    usefulPosts: selectedPosts,
+    totalPosts: totalPosts,
+  });
+});
+
+router.get("/posts/:id", async function (req, res) {
+  query = `SELECT posts.*, authors.name, authors.email FROM posts 
+  INNER JOIN authors ON posts.author_id = authors.id
+  WHERE posts.id = ?`;
+  const result = await db.query(query, [req.params.id]);
+  const [post] = result;
+
+  if (!post || post.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  res.status(200).render("post-detail", { post: post[0] });
 });
 
 router.get("/new-post", async function (req, res) {
